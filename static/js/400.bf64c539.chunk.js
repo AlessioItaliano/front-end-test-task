00f1020e90255dd644906bf967af1d041c4eab79
AlessioItaliano <117004345+AlessioItaliano@users.noreply.github.com>
2024-02-22 21:24:27 +0000

    Deploying to gh-pages from @ AlessioItaliano/front-end-test-task@54e0c11623374d20f56ade8f1e225559bf84381b 🚀

---
+++ static/js/400.bf64c539.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfront_end_test_task=self.webpackChunkfront_end_test_task||[]).push([[400],{400:(t,e,s)=>{s.r(e),s.d(e,{default:()=>a});var n=s(579);const a=()=>(0,n.jsx)(n.Fragment,{})}}]);
+//# sourceMappingURL=400.bf64c539.chunk.js.map
